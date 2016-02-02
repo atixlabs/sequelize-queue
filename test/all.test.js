e9b167ebb0b7f38fa98d448529070c13fb7c370d
Alan Verbner <alverbner@gmail.com>
2016-02-02 18:43:33 -0300

    Export a factory function

--- test/all.test.js
+++ test/all.test.js
@@ -11,7 +11,7 @@ var chai = require('chai'),
 	Sequelize = Support.Sequelize,
 	Promise = Sequelize.Promise,
 	_ = require('lodash'),
-	Queue = require('../lib/');
+	createQueue = require('../lib/');
 
 // init
 chai.use(promised);
@@ -22,7 +22,7 @@ chai.config.includeStack = true;
 describe(Support.getTestDialectTeaser('Tests'), function () {
 	it('It works!', function() {
 		var results = [];
-		var queue = new Queue(this.sequelize, {
+		var queue = createQueue(this.sequelize, {
 			processors: {
 				default: function(data) {
 					return Promise.delay(100).then(function() {
@@ -32,23 +32,23 @@ describe(Support.getTestDialectTeaser('Tests'), function () {
 				}
 			}
 		});
-		
+
 		return queue.init()
 		.then(function() {
 			queue.start();
-			
+
 			return Promise.each([1, 2, 3], function(num) {
 				return queue.addJob({a: num});
 			})
 			.delay(500)
 			.then(function() {
 				expect(results).to.deep.equal([{b: 1}, {b: 2}, {b: 3}]);
-				
+
 				return queue.model.findAll({order: [['id']]})
 				.then(function(results) {
 					expect(results).to.be.ok;
 					expect(results.length).to.equal(3);
-					
+
 					results.forEach(function(result, index) {
 						var data = JSON.parse(result.result);
 						expect(data.b).to.equal(index + 1);
